f288a53ec366954f791d4d6aed5d86134e209493
Giorgos Logiotatidis <seadog@sealabs.net>
2011-03-24 16:33:34 +0200

    timesince function

--- util.py
+++ util.py
@@ -3,10 +3,12 @@ import hashlib
 import librsync
 import tempfile
 import urllib
-from datetime import datetime
+from datetime import datetime, timedelta
 import subprocess
 import pynotify
 from glib import GError
+import time
+
 
 from twisted.web import client
 if __debug__:
@@ -178,3 +180,74 @@ def open_file(path):
     Open 'path' in the user's system configured program
     """
     subprocess.Popen(["xdg-open", "%s" % path])
+
+
+# from django.utils
+# enhanced
+### start
+def ungettext(one, many, howmany):
+    if howmany == 1:
+        return one
+    else:
+        return many
+
+def ugettext(foo):
+    return foo
+
+def timesince(d, now=None):
+    """
+    Takes two datetime objects and returns the time between d and now
+    as a nicely formatted string, e.g. "10 minutes".  If d occurs after now,
+    then "0 minutes" is returned.
+
+    Units used are years, months, weeks, days, hours, and minutes.
+    Seconds and microseconds are ignored.  Up to two adjacent units will be
+    displayed.  For example, "2 weeks, 3 days" and "1 year, 3 months" are
+    possible outputs, but "2 weeks, 3 hours" and "1 year, 5 days" are not.
+
+    Adapted from http://blog.natbat.co.uk/archive/2003/Jun/14/time_since
+    """
+    chunks = (
+      (60 * 60 * 24 * 365, lambda n: ungettext('year', 'years', n)),
+      (60 * 60 * 24 * 30, lambda n: ungettext('month', 'months', n)),
+      (60 * 60 * 24 * 7, lambda n : ungettext('week', 'weeks', n)),
+      (60 * 60 * 24, lambda n : ungettext('day', 'days', n)),
+      (60 * 60, lambda n: ungettext('hour', 'hours', n)),
+      (60, lambda n: ungettext('minute', 'minutes', n))
+    )
+    # Convert datetime.date to datetime.datetime for comparison.
+    if not isinstance(d, datetime):
+        d = datetime(d.year, d.month, d.day)
+    if now and not isinstance(now, datetime):
+        now = datetime(now.year, now.month, now.day)
+
+    if not now:
+        now = datetime.now()
+
+    # ignore microsecond part of 'd' since we removed it from 'now'
+    delta = now - (d - timedelta(0, 0, d.microsecond))
+    since = delta.days * 24 * 60 * 60 + delta.seconds
+    if since <= 0:
+        # d is in the future compared to now, stop processing.
+        # return u'0 ' + ugettext('minutes')
+        return u'now'
+
+    for i, (seconds, name) in enumerate(chunks):
+        count = since // seconds
+        if count != 0:
+            break
+
+    if count == 0 and name(count) == u'minutes':
+        s = ugettext('now')
+    else:
+        s = ugettext('%(number)d %(type)s') % {'number': count, 'type': name(count)}
+        if i + 1 < len(chunks):
+            # Now get the second item
+            seconds2, name2 = chunks[i + 1]
+            count2 = (since - (seconds * count)) // seconds2
+            if count2 != 0:
+                s += ugettext(', %(number)d %(type)s') % {'number': count2, 'type': name2(count2)}
+        s += ugettext(' ago')
+    return s
+
+### end
