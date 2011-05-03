438761cb1add6786d8945f645103a8e39e4fb661
Tasos Katsoulas <akatsoulas@gmail.com>
2011-05-03 18:51:53 +0300

    resolve conflicts

--- melissi/setup.py
+++ melissi/setup.py
@@ -18,42 +18,6 @@ from distutils.command.install_data import install_data
 include_dirs=[]
 version_string='0.1.0'
 
-<<<<<<< HEAD
-_data_files = [('share/man/man1',
-    ['docs/man/man1.txt']),
-    ('share/doc/melissi',
-	['CHANGELOG.txt',
-	'LICENCE.txt',
-	'README.txt'])
-	]
-
-setup(
-    name='melissi',
-    version='0.1.0',
-    author='Giorgos Logiotatidis',
-    author_email='seadog@sealabs.net',
-    url='www.melissi.org',
-    licence='GPLv3',
-    keywords=['file synchronization','client','python'],
-	description='File Synchronization client',
-	long_description=open('README.txt').read(),
-
-	#TODO classifiers for the Pypi
-    data_files=_data_files,
-	package_data = {'melissi':
-        ['data/images/*.svg',
-        'data/glade/*.glade',
-        'data/glade/*.png',
-		'data/glade/*.css',
-		'data/glade/*.html']
-        },
-    packages = find_packages(exclude=['docs','data']),
-#	ext_modules= [Extension('melissi._librsyncmodules',
-#			['_librsyncmodule.c'],
-#			include_dirs,
-#			libraries=["rsync"])],
-    entry_points = {'console_scripts':['melissi = melissi.melissi:main']},
-=======
 _data_files = [
     ('share/man/man1', ['docs/man/man1.txt']),
     ('share/doc/melissi', ['CHANGES.txt',
@@ -93,5 +57,4 @@ setup(
     entry_points = {
         'console_scripts':['melissi = melissi.melissi.mail']
         },
->>>>>>> 01ece62fb67af917abc8fee706745eba0a537104
     )
