cf723c8184464c8ed628db90123fe1b43e106ca5
Tasos Katsoulas <akatsoulas@gmail.com>
2011-09-14 20:26:45 +0300

    cmelissi rewrited with dbus

--- melissi/cmelissi.py
+++ melissi/cmelissi.py
@@ -7,178 +7,32 @@
 # standard modules
 import sys
 import os
-import json
+import dbus
 from optparse import OptionParser
 
-# extra modules
-from twisted.internet import reactor
-from twisted.protocols.basic import LineReceiver
-from twisted.internet.protocol import ClientFactory
-
-modes = {'wara':'1', 'wnra':'2'}
-command_list = []
-
-class MelisiCommanderProtocol(LineReceiver):
-    def connectionMade(self):
-        for cmd in self.factory.command_list:
-            # send command
-            self.sendLine(cmd)
-
-        # force writing before exiting
-        self.transport.doWrite()
-
-    def lineReceived(self, line):
-        print line
-
-    def connectionLost(self, reason):
-        reactor.stop()
-
-class MelisiCommander(ClientFactory):
-    protocol = MelisiCommanderProtocol
-
-    def __init__(self, command_list):
-        self.command_list = command_list
-
-def usage(commands):
-    print "Available commands: " + ', '.join(commands)
-
-def removeshare(args):
-    pass
-
-def addshare(args):
-    def usage():
-        print "Usage: %s addshare [folder] [mode] [user]" % sys.argv[0]
-
-    if len(args) != 3:
-        usage()
-        sys.exit(1)
-
-    # check if proper mode
-    if not (args[1] in modes.keys()):
-        usage()
-        sys.exit(1)
-
-    cmd = {}
-    cmd["command"] = "SHARE"
-    cmd["path"] = os.path.abspath(args[0])
-    cmd["mode"] = modes[args[1]]
-    cmd["user"] = args[2]
-
-
-    command_list.append(json.dumps(cmd))
-
-
-def auth(args):
-    def usage():
-        print "Usage: %s auth [username] [password]" % sys.argv[0]
-
-    if len(args) != 2:
-        usage()
-        sys.exit(1)
-
-    username = args[0]
-    password = args[1]
-    cmd = {}
-    cmd["command"] = "AUTH"
-    cmd["username"] = username
-    cmd["password"] = password
-
-    command_list.append(json.dumps(cmd))
-
-def disconnect(args):
-    cmd = {'command':'DISCONNECT'}
-    command_list.append(json.dumps(cmd))
-
-def connect(args):
-    cmd = {'command':'CONNECT'}
-    command_list.append(json.dumps(cmd))
-
-def register(args):
-    def usage():
-        print "Usage: %s register [username] [password] [email]" % sys.argv[0]
-
-    if len(args) != 3:
-        usage()
-        sys.exit()
-    username = args[0]
-    password = args[1]
-    email = args[2]
-
-    cmd = {'command':'REGISTER',
-           'username':username,
-           'password':password,
-           'password2':password,
-           'email':email}
-    command_list.append(json.dumps(cmd))
-
-def checkbusy(args):
-    cmd = {'command':'CHECKBUSY'}
-    command_list.append(json.dumps(cmd))
-
-def sethost(args):
-    def usage():
-        print "Usage: %s sethost http[s]://[host]:[port]/" % sys.argv[0]
-
-    if len(args) != 1:
-        usage()
-        sys.exit()
-
-    cmd = {'command':'SETHOST',
-           'host':args[0],
-           }
-    command_list.append(json.dumps(cmd))
-
-def deleteuser(args):
-    def usage():
-        print "Usage: %s deleteuser iamsure" % sys.argv[0]
-
-    if len(args) != 1 and args[0] != "iamsure":
-        usage()
-        sys.exit()
-
-    cmd = {'command':'DELETEUSER'}
-    command_list.append(json.dumps(cmd))
-
-def _timeout():
-    """ If I get called, I failed to contact melissi instance """
-    reactor.stop()
-    print "Failed to contact melissi instance"
-    os._exit(1)
-
 def main():
+    bus = dbus.SessionBus()
+    com_services = bus.get_object('org.melissi.Melissi','/org/melissi/Melissi')
+
     parser = OptionParser()
-    parser.add_option("--socket",
-                      help="UNIX socket where melissi client listens",
-                      default=os.path.expanduser("~/.config/melisi/melisi.sock")
-                      )
+#    parser.add_option("--socket",
+#                      help="UNIX socket where melissi client listens",
+#                      default=os.path.expanduser("~/.config/melisi/melisi.sock")
+#                      )
     (options, args) = parser.parse_args()
 
-    commands = {
-        'auth':auth,
-        'disconnect':disconnect,
-        'connect':connect,
-        'register':register,
-        'checkbusy':checkbusy,
-        'sethost':sethost,
-        'deleteuser':deleteuser,
-        'addshare':addshare,
-        'removeshare':removeshare,
-        }
 
     if len(args) < 1:
         usage(commands)
         sys.exit(0)
 
     try:
-        commands[args[0]](args[1:])
-    except KeyError:
-        usage(commands)
+        command = com_services.get_dbus_method(args[0],'org.melissi.Melissi')
+        command(*args[1:])
+    except dbus.DBusException:
+        #TODO fix the exception
         sys.exit(1)
 
-    melisi_commander = MelisiCommander(command_list)
-    reactor.connectUNIX(options.socket, melisi_commander)
-    reactor.callLater(2, _timeout)
-    reactor.run()
 
 if __name__ == '__main__':
     main()
